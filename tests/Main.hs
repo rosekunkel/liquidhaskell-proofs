4ebaae9fb80cd22964cb4b2c36deb6c6f2ad7019
Will Kunkel <will@wkunkel.com>
2017-10-30 19:24:50 -0400

    Add a test to run liquid haskell.

---
+++ tests/Main.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import System.Exit
+import System.Process
+import System.Directory
+
+main :: IO ExitCode
+main =
+  withCurrentDirectory sourceDir $
+  mconcat <$>
+  mapM runLiquid orderedFiles
+
+orderedFiles :: [FilePath]
+orderedFiles =
+  [ "Proofs.hs"
+  , "Types.hs"
+  ]
+
+sourceDir :: FilePath
+sourceDir = "src"
+
+runLiquid :: FilePath -> IO ExitCode
+runLiquid file = spawnProcess "stack" args >>= waitForProcess
+  where args = ["exec", "--", "liquid", file]
+
+instance Monoid ExitCode where
+  mempty = ExitSuccess
+  mappend (ExitFailure i) _ = ExitFailure i 
+  mappend ExitSuccess e = e

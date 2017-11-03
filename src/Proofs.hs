5682154f1b8235bf0b9cf57c678c822629990cb4
Will Kunkel <will@wkunkel.com>
2017-11-03 13:24:38 -0400

    Added proofs of some theorems.

--- src/Proofs.hs
+++ src/Proofs.hs
@@ -1,3 +1,42 @@
+{-@ LIQUID "--higherorder"    @-}
+{-@ LIQUID "--exact-data-con" @-}
+{-@ LIQUID "--automatic-instances=liquidinstances" @-}
 module Proofs where
 
+import Prelude hiding ((++), reverse, length)
+import Language.Haskell.Liquid.ProofCombinators 
 import Types
+
+infixr 5 ++
+
+{-@ infix ++ @-}
+{-@ reflect ++ @-}
+(++) :: L a -> L a -> L a
+Nil ++ ys = ys
+(Cons x xs) ++ ys = Cons x (xs ++ ys)
+
+{-@ reflect reverse @-}
+reverse :: L a -> L a
+reverse Nil = Nil
+reverse (Cons x xs) = reverse xs ++ (Cons x Nil)
+
+{-@ reverseSingletonIdentity :: x:a -> {reverse (Cons x Nil) = Cons x Nil}  @-}
+reverseSingletonIdentity :: a -> Proof
+reverseSingletonIdentity x = trivial
+
+{-@ reflect add @-}
+add :: N -> N -> N
+add Zero m = m
+add (Succ n) m = Succ (add n m)
+
+{-@ addRightIdentityZero :: n:N -> {add n Zero = n} @-}
+addRightIdentityZero :: N -> Proof
+addRightIdentityZero m = case m of
+  Zero -> trivial
+  Succ m' -> addRightIdentityZero m'
+
+{-@ addAssociative :: x:N -> y:N -> z:N -> {add x (add y z) = add (add x y) z} @-}
+addAssociative :: N -> N -> N -> Proof
+addAssociative x y z = case x of
+  Zero -> trivial
+  Succ x' -> addAssociative x' y z
--- src/Types.hs
+++ src/Types.hs
@@ -1,5 +1,30 @@
-module Types (List(..), Nat(..)) where
+{-@ LIQUID "--higherorder"    @-}
+{-@ LIQUID "--exact-data-con" @-}
+{-@ LIQUID "--automatic-instances=liquidinstances" @-}
+module Types ( L(..)
+             , length
+             , N(..)
+             , value
+             ) where
 
-data List a = Nil | Cons a (List a)
+import Prelude hiding (length)
 
-data Nat = Zero | Succ Nat
+{-@ data L [length] @-}
+data L a = Nil | Cons a (L a)
+
+{-@ measure length @-}
+{-@ invariant {v:L a | 0 <= length v} @-}
+{-@ length :: L a -> Nat @-}
+length :: L a -> Int
+length Nil = 0
+length (Cons x xs) = 1 + length xs
+
+{-@ data N [value] @-}
+data N = Zero | Succ N
+
+{-@ measure value @-}
+{-@ invariant {v:N | 0 <= value v} @-}
+{-@ value :: N -> Nat @-}
+value :: N -> Int
+value Zero = 0
+value (Succ n) = 1 + value n

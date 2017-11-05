7de2a0a069dcc17fb8c3d8d00e4475bf0a085477
Will Kunkel <will@wkunkel.com>
2017-11-05 13:35:54 -0500

    Add flatten proof.

--- src/Flatten.hs
+++ src/Flatten.hs
@@ -8,6 +8,40 @@ import Language.Haskell.Liquid.ProofCombinators
 import Types hiding (L(..), length)
 import Data.List.Verified
 
+{-@ infixr 5 ++ @-}
+
+{-@ reflect flatten @-}
 flatten :: Tree -> List Int
 flatten (Leaf n) = n:::Nil
 flatten (Node l r) = flatten l ++ flatten r
+
+{-@ reflect flatten' @-}
+flatten' :: Tree -> List Int -> List Int
+flatten' (Leaf n) ns = n:::ns
+flatten' (Node l r) ns = flatten' l (flatten' r ns)
+
+{-@ reflect flatten'' @-}
+flatten'' :: Tree -> List Int
+flatten'' t = flatten' t Nil
+
+{-@ flatten'Appends :: t:Tree -> ns:List Int -> { flatten' t ns = flatten t ++ ns } @-}
+flatten'Appends :: Tree -> List Int -> Proof
+flatten'Appends t ns = case t of
+  Leaf _ -> trivial
+  Node l r ->
+    flatten' t ns
+    ==. flatten' l (flatten' r ns)
+    ==. flatten' l (flatten r ++ ns) ? flatten'Appends r ns
+    ==. flatten l ++ (flatten r ++ ns) ? flatten'Appends l (flatten r ++ ns)
+    ==. (flatten l ++ flatten r) ++ ns ? appendAssoc (flatten l) (flatten r) ns
+    ==. flatten t ++ ns
+    *** QED
+
+{-@ flatten''Correct :: t:Tree -> { flatten'' t = flatten t } @-}
+flatten''Correct :: Tree -> Proof
+flatten''Correct t =
+  flatten'' t
+  ==. flatten' t Nil
+  ==. flatten t ++ Nil ? flatten'Appends t Nil
+  ==. flatten t ? appendRightId (flatten t)
+  *** QED

507023a1c9a4b0ecc0b0371106b18764e8d340f7
Will Kunkel <will@wkunkel.com>
2017-11-04 21:34:26 -0400

    Add replicate proof.

--- src/Replicate.hs
+++ src/Replicate.hs
@@ -7,7 +7,15 @@ import Prelude hiding (replicate, length)
 import Language.Haskell.Liquid.ProofCombinators
 import Types
 
+{-@ reflect replicate @-}
 {-@ replicate :: Nat -> a -> L a @-}
 replicate :: Int -> a -> L a
-replicate 0 _ = Nil
-replicate n x = Cons x (replicate (n - 1) x)
+replicate n x
+  | n == 0 = Nil
+  | otherwise = Cons x (replicate (n - 1) x)
+
+{-@ replicateLengthCorrect :: n:Nat -> x:a -> { length (replicate n x) = n } @-}
+replicateLengthCorrect :: Int -> a -> Proof
+replicateLengthCorrect n x = case n of
+  0 -> trivial
+  n -> replicateLengthCorrect (n - 1) x
